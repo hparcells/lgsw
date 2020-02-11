8c10ecbd9297ea7d8ab73d4cdbcc84acb24379b9
Hunter Parcells <hparcells@icloud.com>
2020-02-10 19:55:03 -0500

    removed camera

--- src/client/logic/logic.ts
+++ src/client/logic/logic.ts
@@ -2,60 +2,35 @@ import { updateInputManager, mouse, keyboard } from 'easy-web-input';
 
 import { GameState } from '../types';
 
-export const GRID_SIZE = 128;
+export const GRID_SIZE = 64;
 
 export let canvas: HTMLCanvasElement;
 export let ctx: CanvasRenderingContext2D;
 
 let state: GameState = {
-  objects: [],
-  camera: {
-    x: 0,
-    y: 0,
-    scale: 0.5
-  }
+  objects: []
 }
+
+let lastClick = { x: 0, y: 0 };
+let previousCameraPosition = { x: 0, y: 0 };
 
 /** The game loop. */
 function gameLoop() {
+  // Clean the entire canvas.
   ctx.fillStyle = '#000000';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Handle arrow keys.
-  if(keyboard.ArrowLeft) {
-    state.camera.x -= 5;
-  }
-  // Handle arrow keys.
-  if(keyboard.ArrowRight) {
-    state.camera.x += 5;
-  }
-  if(keyboard.ArrowUp) {
-    state.camera.y -= 5;
-  }
-  if(keyboard.ArrowDown) {
-    state.camera.y += 5;
-  }
-
-  // Mouse movement.
-  if(mouse.right) {
-    // TODO:
-  }
-
+  
   // Resize the canvas when we resize the canvas.
   window.addEventListener('resize', () => {
     ctx.canvas.width  = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
   });
 
-  ctx.save();
-
-  ctx.translate(-(state.camera.x % GRID_SIZE) - GRID_SIZE * 2, -(state.camera.y % GRID_SIZE) - GRID_SIZE);
-
   // Draw the grid.
   // For each row.
-  for(let i = 0; i < Math.ceil(canvas.height / (GRID_SIZE * state.camera.scale)) * 1.5; i++) {
+  for(let i = 0; i < Math.ceil(canvas.height / GRID_SIZE); i++) {
     // For each column.
-    for(let j = 0; j < Math.ceil(canvas.width / (GRID_SIZE * state.camera.scale)) * 1.5; j++) {
+    for(let j = 0; j < Math.ceil(canvas.width / GRID_SIZE); j++) {
       if(i % 2 === 0 && j % 2 === 0) {
         ctx.fillStyle = '#757575';
       }else if(i % 2 === 0) {
@@ -64,31 +39,40 @@ function gameLoop() {
         ctx.fillStyle = '#A0A0A0';
       }else {
         ctx.fillStyle = '#757575';
-      }    
+      }
       
       // Draw something/
       ctx.fillRect(
-        (j * (GRID_SIZE * state.camera.scale)),
-        (i * (GRID_SIZE * state.camera.scale)),
-        GRID_SIZE * state.camera.scale,
-        GRID_SIZE * state.camera.scale
+        j * GRID_SIZE,
+        i * GRID_SIZE,
+        GRID_SIZE,
+        GRID_SIZE
       );
     }
   }
+  
+  // Draw the hovered tile.
+  // For each row.
+  for(let i = 0; i < Math.ceil(canvas.height / GRID_SIZE * 1.5); i++) {
+    // For each column.
+    for(let j = 0; j < Math.ceil(canvas.width / GRID_SIZE) * 1.5; j++) {
+      if(
+        mouse.x >= j * GRID_SIZE
+        && mouse.x <= j * GRID_SIZE + GRID_SIZE
+        && mouse.y >= i * GRID_SIZE
+        && mouse.y <= i * GRID_SIZE + GRID_SIZE
+      ) {
+        ctx.fillStyle = 'rgba(0, 255, 0, 0.5)';
 
-  ctx.restore();
-
-  // Save camera settings.
-  ctx.save();
-
-  ctx.translate(-state.camera.x, -state.camera.y);
-
-  ctx.fillStyle = '#FF0000';
-  ctx.fillRect(-GRID_SIZE, -GRID_SIZE, GRID_SIZE * state.camera.scale, GRID_SIZE * state.camera.scale);
-  ctx.fillRect(-GRID_SIZE, -GRID_SIZE * 2, GRID_SIZE * state.camera.scale, GRID_SIZE * state.camera.scale);
-
-  // Restore camera settings..
-  ctx.restore();
+        ctx.fillRect(
+          j * GRID_SIZE,
+          i * GRID_SIZE,
+          GRID_SIZE,
+          GRID_SIZE
+        );
+      }
+    }
+  }
 
   // Update input.
   updateInputManager();
--- src/client/types.ts
+++ src/client/types.ts
@@ -1,10 +1,3 @@
-interface Camera {
-  x: number;
-  y: number;
-  scale: number;
-}
-
 export interface GameState {
   objects: any[];
-  camera: Camera;
 }
